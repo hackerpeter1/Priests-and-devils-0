1fda6f1c2f6d283d9b4155b36717e78791e7d13e
延泽 许 <peter13994@126.com>
2018-04-01 11:21:39 +0800

    homework

---
+++ Stars/Assets/Scripts/RotateAround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateAround : MonoBehaviour {
+    public Transform target;
+    public float speed = 1.0f;
+    private float rx, ry;
+	// Use this for initialization
+	void Start () {
+        rx = Random.Range(10, 20);
+        ry = Random.Range(80, 90);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 axis = new Vector3(rx,ry,0);
+        this.transform.RotateAround(target.position, axis, Time.deltaTime * 5 * speed);
+	}
+}
